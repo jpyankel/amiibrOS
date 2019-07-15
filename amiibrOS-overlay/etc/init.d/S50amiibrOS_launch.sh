f380ac703fde463c69d38a08c153a036c6b03d45
jpyankel <jpyankel@gmail.com>
2019-07-14 21:06:29 -0400

    Add init.d Script to Overlay; Update amiibrOS-buildroot Version

---
+++ amiibrOS-overlay/etc/init.d/S50amiibrOS_launch.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+# /etc/init.d/amiibrOS_launch.sh
+#
+# description: amiibrOS init script
+# processname: amiibrOS_init
+#
+# Joseph Yankel (jpyankel)
+
+start() {
+  echo "starting amiibrOS..."
+  cd /usr/bin/amiibrOS && ./amiibrOS
+}
+
+case "$1" in
+    start)
+  start
+  ;;
+    *)
+  echo "Usage: $0 {start}"
+	exit 1
+  ;;
+esac
+
+exit 0
