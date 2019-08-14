a0d6cc3299cc2f575b64b143d03f84d8f4a8f9f9
Joseph <jpyankel@gmail.com>
2019-08-14 15:02:14 -0400

    Add LED Status Indicator to powerswitch.py

--- subproj/powerswitch/powerswitch.py
+++ subproj/powerswitch/powerswitch.py
@@ -7,6 +7,8 @@
   Hold the power switch for SHUTDOWN_HOLD_TIME to turn RPI off. Press for less
     than that duration to reboot.
 
+  Also enables a power-status LED indicator on LED_PIN.
+
   Joseph Yankel (jpyankel@gmail.com)
 """
 
@@ -15,6 +17,7 @@ import RPi.GPIO as GPIO
 
 SHUTDOWN_HOLDTIME = 3.0 # Number of seconds we need to hold to shutdown
 WAKE_PIN = 5 # GPIO Pin which we use for wake/shutdown functionality
+LED_PIN = 11 # GPIO Pin which we use for the power-status indicator
 
 # Mutex used to prevent both threads from executing shutdown/reboot at once:
 shutdown_lock = threading.Lock()
@@ -29,6 +32,9 @@ def main():
 	
   # Set wake pin to be an input pin pulled to high (3.3V):
   GPIO.setup(WAKE_PIN, GPIO.IN)
+  # Set led indicator to on since we are booted up
+  GPIO.setup(LED_PIN, GPIO.OUT)
+  GPIO.output(LED_PIN, GPIO.HIGH)
 	
   # Wait for wake pin to be brought low:
   GPIO.wait_for_edge(WAKE_PIN, GPIO.FALLING)
@@ -47,16 +53,26 @@ def main():
 def reboot():
   # Make sure this thread is the only one performing reboot/shutdown:
   shutdown_lock.acquire()
+
+  # Set led indicator to off since we are rebooting:
+  GPIO.output(LED_PIN, GPIO.LOW)
+
   # Best practice to perform cleanup before program termination:
   GPIO.cleanup()
+
   # Reboot:
   subprocess.call('reboot', shell=False)
 
 def shutdown():
   # Make sure this thread is the only one performing reboot/shutdown:
   shutdown_lock.acquire()
+
+  # Set led indicator to off since we are shutting down:
+  GPIO.output(LED_PIN, GPIO.LOW)
+
   # Best practice to perform cleanup before program termination:
   GPIO.cleanup()
+
   # Shutdown:
   subprocess.call('poweroff', shell=False)
 
